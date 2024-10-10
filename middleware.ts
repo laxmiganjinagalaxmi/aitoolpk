8110601dd61cfbb2f1b7b12b2adc4d2cd4fc1c7f
laxmiganjinagalaxmi <148781354+laxmiganjinagalaxmi@users.noreply.github.com>
2024-10-10 07:27:19 +0530

    kk

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+   publicRoutes: ["/"]
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+
+
